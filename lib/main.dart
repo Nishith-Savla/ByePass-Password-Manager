9792607883554e1f3d6ab2494b4e2bcd467356a0
Nishith.Savla <nishithsavla005@gmail.com>
2021-11-08 20:57:52 +0530

    :lipstick: Create a purple color theme for primary swatch

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,26 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'pages/login.dart';
 
+const purpleMaterialColor = MaterialColor(0xFFA633FF, {
+  50: Color.fromRGBO(166, 51, 255, .1),
+  100: Color.fromRGBO(166, 51, 255, .2),
+  200: Color.fromRGBO(166, 51, 255, .3),
+  300: Color.fromRGBO(166, 51, 255, .4),
+  400: Color.fromRGBO(166, 51, 255, .5),
+  500: Color.fromRGBO(166, 51, 255, .6),
+  600: Color.fromRGBO(166, 51, 255, .7),
+  700: Color.fromRGBO(166, 51, 255, .8),
+  800: Color.fromRGBO(166, 51, 255, .9),
+  900: Color.fromRGBO(166, 51, 255, 1),
+});
+
 void main() async {
   await dotenv.load(fileName: ".env");
 
-  runApp(const MaterialApp(home: Login()));
+  runApp(MaterialApp(
+    home: const Login(),
+    theme: ThemeData(
+      primarySwatch: purpleMaterialColor,
+    ),
+  ));
 }
