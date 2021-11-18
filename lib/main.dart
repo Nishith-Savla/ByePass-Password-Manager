c9757f83384c272929cc1e728bbf94a20645af47
Jay-Salunke <jysl2004@gmail.com>
2021-11-18 12:08:49 +0530

    :art: Move global materialcolor to utils

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-import 'pages/login.dart';
-
-const purpleMaterialColor = MaterialColor(0xFFA633FF, {
-  50: Color.fromRGBO(166, 51, 255, .1),
-  100: Color.fromRGBO(166, 51, 255, .2),
-  200: Color.fromRGBO(166, 51, 255, .3),
-  300: Color.fromRGBO(166, 51, 255, .4),
-  400: Color.fromRGBO(166, 51, 255, .5),
-  500: Color.fromRGBO(166, 51, 255, .6),
-  600: Color.fromRGBO(166, 51, 255, .7),
-  700: Color.fromRGBO(166, 51, 255, .8),
-  800: Color.fromRGBO(166, 51, 255, .9),
-  900: Color.fromRGBO(166, 51, 255, 1),
-});
+import 'package:password_manager/utils.dart';
+import 'package:password_manager/pages/login.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
--- lib/utils.dart
+++ lib/utils.dart
@@ -1,2 +1,17 @@
+import 'package:flutter/material.dart';
+
 String emailRegex =
     r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+
+const purpleMaterialColor = MaterialColor(0xFFA633FF, {
+  50: Color.fromRGBO(166, 51, 255, .1),
+  100: Color.fromRGBO(166, 51, 255, .2),
+  200: Color.fromRGBO(166, 51, 255, .3),
+  300: Color.fromRGBO(166, 51, 255, .4),
+  400: Color.fromRGBO(166, 51, 255, .5),
+  500: Color.fromRGBO(166, 51, 255, .6),
+  600: Color.fromRGBO(166, 51, 255, .7),
+  700: Color.fromRGBO(166, 51, 255, .8),
+  800: Color.fromRGBO(166, 51, 255, .9),
+  900: Color.fromRGBO(166, 51, 255, 1),
+});
