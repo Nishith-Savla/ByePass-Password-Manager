ecaff7241087278f40a6244775d338eb7b05c234
Nishith.Savla <nishithsavla005@gmail.com>
2021-11-23 19:08:33 +0530

    :sparkles: Add the signup screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:password_manager/pages/login.dart';
+import 'package:password_manager/pages/signup.dart';
 import 'package:password_manager/utils.dart';
 
 void main() async {
@@ -11,7 +11,7 @@ void main() async {
   await dotenv.load(fileName: ".env");
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: const Login(),
+    home: const Signup(),
     theme: ThemeData(
       primarySwatch: purpleMaterialColor,
     ),
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -5,41 +5,52 @@ import 'package:password_manager/components/rounded_button.dart';
 import 'package:password_manager/components/rounded_textfield.dart';
 import 'package:password_manager/firebase/authentication.dart';
 
-class Login extends StatefulWidget {
-  const Login({Key? key}) : super(key: key);
+class Signup extends StatefulWidget {
+  const Signup({Key? key}) : super(key: key);
 
   @override
-  _LoginState createState() => _LoginState();
+  _SignupState createState() => _SignupState();
 }
 
-class _LoginState extends State<Login> {
+class _SignupState extends State<Signup> {
   final _formKey = GlobalKey<FormState>();
+
+  bool _isPasswordVisible = true;
+  bool _isPasswordFocused = false;
+
+  String _name = "";
   String _email = "";
   String _password = "";
-  bool _isPasswordVisible = false;
-  bool _isPasswordFocused = false;
+  String _confirmPassword = "";
+
+  String _nameErrorMessage = "";
   String _emailErrorMessage = "";
   String _passwordErrorMessage = "";
+  String _confirmPasswordErrorMessage = "";
 
   bool _validate() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+      debugPrint(_name);
       debugPrint(_email);
       debugPrint(_password);
+      debugPrint(_confirmPassword);
       return true;
     }
     return false;
   }
 
-  void _login() async {
+  void _signup() async {
     if (!_validate()) return;
     final auth = Authentication();
-    final authResult = await auth.signInWithEmailAndPassword(
-        email: _email, password: _password);
-    final error = authResult.error;
-    if (error != null) {
+    final error = await auth.addUserWithEmailAndPassword(
+        email: _email, password: _password, collectionPath: 'users');
+    if (error != null && error.isNotEmpty) {
       ScaffoldMessenger.of(context)
           .showSnackBar(SnackBar(content: Text(error)));
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text('Sign up successful')));
     }
   }
 
@@ -55,24 +66,57 @@ class _LoginState extends State<Login> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const Text(
-                  "LOGIN",
+                  "SIGN UP",
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 20,
                   ),
                 ),
+
+                //
                 SizedBox(
-                  height: size.height * 0.03,
+                  height: size.height * 0.02,
                 ),
                 SvgPicture.asset(
                   "assets/icons/login.svg",
-                  height: size.height * 0.35,
+                  height: size.height * 0.25,
                 ),
                 SizedBox(
-                  height: size.height * 0.03,
+                  height: size.height * 0.02,
                 ),
+
+                // Name
                 Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 3),
+                  padding: const EdgeInsets.symmetric(vertical: 2),
+                  child: Column(
+                    children: [
+                      RoundedTextFormField(
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        keyboardType: TextInputType.name,
+                        labelText: "Enter you name",
+                        autofillHints: const [AutofillHints.name],
+                        icon: Icons.person,
+                        validator: (name) {
+                          WidgetsBinding.instance!.addPostFrameCallback(
+                            (_) => setState(() => _nameErrorMessage =
+                                name!.isNotEmpty ? '' : 'Name cannot be empty'),
+                          );
+                        },
+                        onSaved: (name) => _name = name!,
+                      ),
+                      _nameErrorMessage.isNotEmpty
+                          ? Text(
+                              _nameErrorMessage,
+                              style: const TextStyle(color: Colors.red),
+                            )
+                          : Container(),
+                    ],
+                  ),
+                ),
+
+                // Email
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 2),
                   child: Column(
                     children: [
                       RoundedTextFormField(
@@ -94,14 +138,17 @@ class _LoginState extends State<Login> {
                       _emailErrorMessage.isNotEmpty
                           ? Text(
                               _emailErrorMessage,
+                              textAlign: TextAlign.left,
                               style: const TextStyle(color: Colors.red),
                             )
                           : Container(),
                     ],
                   ),
                 ),
+
+                // Password
                 Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 5.0),
+                  padding: const EdgeInsets.symmetric(vertical: 2),
                   child: Column(
                     children: [
                       Focus(
@@ -142,21 +189,71 @@ class _LoginState extends State<Login> {
                       _passwordErrorMessage.isNotEmpty
                           ? Text(
                               _passwordErrorMessage,
-                              style: const TextStyle(
-                                color: Colors.red,
-                              ),
+                              style: const TextStyle(color: Colors.red),
                             )
                           : Container(),
                     ],
                   ),
                 ),
+
+                // Confirm Password
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 2),
+                  child: Column(
+                    children: [
+                      Focus(
+                        child: RoundedTextFormField(
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          labelText: "Confirm your password",
+                          icon: Icons.lock,
+                          obscureText: _isPasswordVisible,
+                          keyboardType: TextInputType.visiblePassword,
+                          autofillHints: const [AutofillHints.password],
+                          suffixIcon: _isPasswordFocused
+                              ? IconButton(
+                                  onPressed: () {
+                                    setState(() => _isPasswordVisible =
+                                        !_isPasswordVisible);
+                                  },
+                                  icon: Icon(
+                                    _isPasswordVisible
+                                        ? Icons.visibility_off_outlined
+                                        : Icons.visibility_outlined,
+                                    size: 20,
+                                  ),
+                                )
+                              : null,
+                          validator: (confirmPassword) {
+                            WidgetsBinding.instance!.addPostFrameCallback(
+                              (_) => setState(() =>
+                                  _confirmPasswordErrorMessage =
+                                      confirmPassword!.isNotEmpty
+                                          ? ''
+                                          : 'Confirm Password cannot be empty'),
+                            );
+                          },
+                          onSaved: (confirmPassword) =>
+                              _confirmPassword = confirmPassword!,
+                        ),
+                        onFocusChange: (hasFocus) {},
+                      ),
+                      _confirmPasswordErrorMessage.isNotEmpty
+                          ? Text(
+                              _confirmPasswordErrorMessage,
+                              style: const TextStyle(color: Colors.red),
+                            )
+                          : Container(),
+                    ],
+                  ),
+                ),
+
                 RoundedButton(
-                  text: "LOGIN",
-                  onPressed: _login,
+                  text: "SIGN UP",
+                  onPressed: _signup,
                 ),
                 SizedBox(height: size.height * 0.02),
                 const Text(
-                  "Don't have an account? Sign Up",
+                  "Already have an account? Login",
                   style: TextStyle(
                     fontSize: 15,
                   ),
