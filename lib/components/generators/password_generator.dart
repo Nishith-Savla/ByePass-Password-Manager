928a1c35009f399247c67656b450c8f06325171d
Nishith.Savla <nishithsavla005@gmail.com>
2021-11-29 23:44:14 +0530

    :bug: Fix bug in password generator
    Fix the bug that the generated password sometimes won't contain numbers or special characters even if stated to do so

--- lib/components/generators/password_generator.dart
+++ lib/components/generators/password_generator.dart
@@ -5,28 +5,57 @@ class PasswordGenerator {
     this.length, {
     this.addLowercaseLetters = true,
     this.addUppercaseLetters = true,
-    this.addNumber = true,
-    this.addSpecial = true,
-  });
+    this.minNumbers = 1,
+    this.minSpecialChars = 1,
+  }) : _random = Random.secure();
 
-  final int length;
-  final bool addLowercaseLetters;
-  final bool addUppercaseLetters;
-  final bool addNumber;
-  final bool addSpecial;
+  int length;
+  bool addLowercaseLetters;
+  bool addUppercaseLetters;
+  int minNumbers;
+  int minSpecialChars;
+  final Random _random;
 
   String generate() {
-    final allowedChars = StringBuffer()
-      ..write((addLowercaseLetters) ? "abcdefghijklmnopqrstuvwxyz" : null)
-      ..write((addUppercaseLetters) ? "ABCDEFGHIJKLMNOPQRSTUVWXYZ" : null)
-      ..write((addNumber) ? '0123456789' : null)
-      ..write((addSpecial) ? r'@#%^*>\$@?/[]=+' : null);
+    const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = '0123456789';
+    const specialChars = r'!@#$%^&*()-+=';
+
+    final generated = <String>[];
+
+    final allowedChars = StringBuffer();
+    if (addLowercaseLetters) {
+      generated.add(lowercaseLetters[_random.nextInt(lowercaseLetters.length)]);
+      allowedChars.write(lowercaseLetters);
+    }
+    if (addUppercaseLetters) {
+      generated.add(uppercaseLetters[_random.nextInt(uppercaseLetters.length)]);
+      allowedChars.write(uppercaseLetters);
+    }
+
+    if (minNumbers > 0) {
+      allowedChars.write(numbers);
+      for (int i = 0; i < minNumbers; ++i) {
+        generated.add(numbers[_random.nextInt(numbers.length)]);
+      }
+    }
+
+    if (minSpecialChars > 0) {
+      allowedChars.write(specialChars);
+      for (int i = 0; i < minSpecialChars; ++i) {
+        generated.add(specialChars[_random.nextInt(specialChars.length)]);
+      }
+    }
 
     final charsString = allowedChars.toString();
-    final random = Random.secure();
 
-    return List<String>.generate(
-            length, (index) => charsString[random.nextInt(allowedChars.length)])
-        .join('');
+    for (int i = generated.length; i < length; ++i) {
+      generated.add(charsString[_random.nextInt(charsString.length)]);
+    }
+
+    generated.shuffle(_random);
+
+    return generated.join();
   }
 }
