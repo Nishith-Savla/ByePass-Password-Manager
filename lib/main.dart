a9deb7f904b7126396ef86cd6f9561c4defdbc1f
Jay-Salunke <jysl2004@gmail.com>
2021-12-10 13:40:06 +0530

    :sparkles: Add routes

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:password_manager/constants.dart';
-import 'package:password_manager/screens/generate.dart';
+import 'package:password_manager/firebase/authentication.dart';
+import 'package:password_manager/routes_generator.dart';
 import 'package:password_manager/screens/home.dart';
-import 'package:password_manager/screens/signup.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
   await dotenv.load(fileName: ".env");
+  final auth = Authentication();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: const HomePage(),
@@ -19,5 +20,11 @@ void main() async {
       fontFamily: GoogleFonts.poppins().fontFamily,
       primarySwatch: purpleMaterialColor,
     ),
+    initialRoute: auth.isUserLoggedIn()
+        ? auth.isEmailVerified()
+            ? "/home"
+            : "/verifyEmail"
+        : "/login",
+    onGenerateRoute: RoutesGenerator.generateRoute,
   ));
 }
--- lib/routes_generator.dart
+++ lib/routes_generator.dart
@@ -1,1 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:password_manager/screens/generate.dart';
+import 'package:password_manager/screens/home.dart';
+import 'package:password_manager/screens/login.dart';
+import 'package:password_manager/screens/signup.dart';
+import 'package:password_manager/screens/verify_email.dart';
 
+class RoutesGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+    switch (settings.name) {
+      case '/home':
+        return MaterialPageRoute(builder: (_) => const HomePage());
+      case '/signup':
+        return MaterialPageRoute(builder: (_) => const Signup());
+      case '/login':
+        return MaterialPageRoute(builder: (_) => const Login());
+      case '/verifyEmail':
+        return MaterialPageRoute(builder: (_) => const VerifyEmail());
+      case '/generate':
+        if (args is GenerateType) {
+          return MaterialPageRoute(
+            builder: (_) => Generate(generateType: args),
+          );
+        }
+        return _errorRoute();
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return const Scaffold(
+        body: Center(
+          child: Text('Screen not available'),
+        ),
+      );
+    });
+  }
+}
