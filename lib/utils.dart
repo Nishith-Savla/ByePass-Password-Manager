2a1d2c56ed0142c63532a2418b8673ccc6107454
Nishith-Savla <nishithsavla005@gmail.com>
2021-12-16 17:32:32 +0530

    :sparkles: Add copyToClipboard() & add show combinators

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,19 +1,33 @@
-import 'dart:convert';
-import 'dart:typed_data';
+import 'dart:convert' show utf8;
+import 'dart:typed_data' show Uint8List;
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:crypto/crypto.dart';
-import 'package:encrypt/encrypt.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart' show Timestamp;
+import 'package:crypto/crypto.dart' show Hmac, sha512256;
+import 'package:encrypt/encrypt.dart' show Key;
+import 'package:flutter/material.dart'
+    show BuildContext, ScaffoldMessenger, SnackBar, Text;
+import 'package:flutter/services.dart' show Clipboard, ClipboardData;
+import 'package:flutter_dotenv/flutter_dotenv.dart' show dotenv;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart'
+    show AndroidOptions, FlutterSecureStorage;
 
 Uint8List generateKey(String password, String pepper, Timestamp createdAt) {
   final key = utf8.encode(password + pepper + createdAt.toString());
   final hmacSha512256 = Hmac(sha512256, key);
-  final ekey =
-      Key(hmacSha512256.convert(utf8.encode(password)).bytes as Uint8List)
-          .bytes;
-  return ekey;
+  return Key(hmacSha512256.convert(utf8.encode(password)).bytes as Uint8List)
+      .bytes;
+}
+
+void copyToClipboard({
+  required BuildContext context,
+  required String name,
+  required String data,
+}) {
+  Clipboard.setData(ClipboardData(text: data)).then((_) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text("$name has been copied to clipboard")),
+    );
+  });
 }
 
 // Local storage
