dfced8817bd08f5ac265841bd35067de0124843a
Nishith-Savla <nishithsavla005@gmail.com>
2021-12-17 01:01:09 +0530

    :sparkles: Restyle the verify email page & add button to open mail on android

--- lib/screens/verify_email.dart
+++ lib/screens/verify_email.dart
@@ -1,6 +1,9 @@
-import 'dart:async';
+import 'dart:async' show Timer;
+import 'dart:io' show Platform;
 
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:android_intent_plus/android_intent.dart' show AndroidIntent;
+import 'package:android_intent_plus/flag.dart' show Flag;
+import 'package:firebase_auth/firebase_auth.dart' show FirebaseAuth, User;
 import 'package:flutter/material.dart';
 import 'package:password_manager/firebase/authentication.dart';
 
@@ -30,10 +33,44 @@ class _VerifyEmailState extends State<VerifyEmail> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-          'An verification link has been to your ${user!.email} email account. \n'
-          'Please open it and once done, switch back to the App'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Verify your email'),
+        automaticallyImplyLeading: false,
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Text('An verification link has been to ${user!.email}. \n'
+                'Please open it and once done, switch back to the App'),
+            ElevatedButton(
+                child: const Text('Open Mail'),
+                onPressed: () async {
+                  if (Platform.isAndroid) {
+                    AndroidIntent intent = const AndroidIntent(
+                      action: 'android.intent.action.MAIN',
+                      category: 'android.intent.category.APP_EMAIL',
+                      flags: [Flag.FLAG_ACTIVITY_NEW_TASK],
+                    );
+                    await intent
+                        .launch()
+                        .then((_) => debugPrint("success"))
+                        .catchError((e) {
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(SnackBar(content: Text(e.toString())));
+                    });
+                    // } else if (Platform.isIOS) {
+                    //   launch("message://").catchError((e) {
+                    //     ScaffoldMessenger.of(context)
+                    //         .showSnackBar(SnackBar(content: e));
+                    //   });
+                  }
+                })
+          ],
+        ),
+      ),
     );
   }
 }
